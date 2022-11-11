4d37e3cfb894b9db38f46d33e1a0f5bc51983ec0
Renge <renge@renge.io>
2022-11-11 17:25:06 -0500

    fix: mongodb

--- auth/db/index.ts
+++ auth/db/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL, {dbName: process.env.MONGO_DBNAME, user: process.env.MONGO_USER, pass: process.env.MONGO_PASS})
     .then(() => {
         console.log('Connected to database')
     })
