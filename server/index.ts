0d440ee52701c7ca18a7886a715a5ee9e795dc3c
David Huang <huangd1224@gmail.com>
2022-11-13 23:03:30 -0500

    test

--- server/index.ts
+++ server/index.ts
@@ -260,7 +260,7 @@ const mediaUpload = async (req, res) => {
         }
         
         let file = req.body;
-        console.log(req.files);
+        console.log(req.body);
         file = await fileToBinary(file);
         console.log(file);
         const contentType = req.header('content-type');
