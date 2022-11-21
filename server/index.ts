5b9859f47a54da2be181c9859c203f7b86dca989
Renge <renge@renge.io>
2022-11-21 01:41:48 -0500

    fix: performance

--- server/index.ts
+++ server/index.ts
@@ -609,25 +609,26 @@ const op = async (req, res) => {
             return res.status(200).send({ error: true, message: "Missing document id" });
         }
 
-        console.log("Doc " + id + " receives Update: " + update)
+        // console.log("Doc " + id + " receives Update: " + update)
         const ydoc = ydocs.get(id);
         if (ydoc) {
-            console.log("Found doc " + id)
-            console.log("Text before update: " + ydoc.doc.getText().toString())
+            // console.log("Found doc " + id)
+            // console.log("Text before update: " + ydoc.doc.getText().toString())
             Y.applyUpdate(ydoc.doc, Uint8Array.from(update.split(',').map(x => parseInt(x, 10))));
-            console.log("Text after update: " + ydoc.doc.getText().toString())
+            // console.log("Text after update: " + ydoc.doc.getText().toString())
 
             await elasticUpdateDoc(ydoc.name, ydoc.doc.getText(), id);
             // TODO: check error
+            res.send({});
 
             addToRecent({ name: ydoc.name, id: id })
-            ydoc.clients.forEach((client, key) => {
+            return ydoc.clients.forEach((client, key) => {
                 client.response.write("event: update\ndata: " + update + "\n\n");
-                console.log("Sending update to client " + key)
+                // console.log("Sending update to client " + key)
             });
-            return setTimeout(function(){
-                res.send({});
-            }, 200);
+            // return setTimeout(function(){
+                
+            // }, 200);
         }
         else {
             console.log("Fail to find doc " + id)
