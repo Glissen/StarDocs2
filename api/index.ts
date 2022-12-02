4667ed1775e09db1f1753ee4aebed8c41098911c
David Huang <huangd1224@gmail.com>
2022-12-02 17:11:06 -0500

    Testing

--- api/index.ts
+++ api/index.ts
@@ -171,7 +171,7 @@ const elasticSuggest = async(query: string) => {
 
 const getUserNameAndId = async (cookie) => {
     try {
-        const result = await axios.post('http://localhost:4001/users/getUserNameAndId', {
+        const result = await axios.post('http://127.0.0.1:4001/users/getUserNameAndId', {
             cookie: cookie
         })
         return { name: result.data.name, id: result.data.id }
